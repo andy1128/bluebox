8fde3d4e00f3ca1c0036ca4c4d7f92fa712a21e2
Concerned-HumanDev <48697078+Concerned-HumanDev@users.noreply.github.com>
2019-04-24 23:24:47 -0400

    Sergio broke the database.

--- bluebox_rework/catalog/models.py
+++ bluebox_rework/catalog/models.py
@@ -11,7 +11,7 @@ from django.forms import ModelForm
 # Create your models here.
 # Model representing a movie.
 class Movie(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Title')
+    title = models.CharField(max_length=200, verbose_name="Title")
     year = models.IntegerField(verbose_name='Year')
     actors = models.CharField(max_length=1000, verbose_name="Actors")
     director = models.CharField(max_length=200, verbose_name="Director")
@@ -20,7 +20,7 @@ class Movie(models.Model):
     img = models.CharField(max_length=300, verbose_name="Image" )
     img_r = models.CharField(max_length=300, verbose_name="Image R" )
     hd_link = models.URLField(verbose_name='HD Link')
-    
+
     # __str__() method to return a human-redable string for each object.
     def __str__(self):
         return self.title
