612b7ff0bf22a690719c73fc0b5892fda5fa3b50
Concerned-HumanDev <48697078+Concerned-HumanDev@users.noreply.github.com>
2019-05-01 13:24:02 -0400

    Sergio fixed issue were DB entries had extra spaces in random places.

--- bluebox_rework/catalog/script.py
+++ bluebox_rework/catalog/script.py
@@ -1,10 +1,13 @@
-import os
-from . models import Movie
+# import os
+from  catalog.models import Movie
 
 #TODO: change path
 with open('C:/Users/Juan/Desktop/movies.txt') as movie_file:
     for line in movie_file:
-        print(line)
         cur_movie = line.split(', ')
+        cur_movie[8] = str(cur_movie[8].replace("\n", ""))
+        cur_movie[2] = str(cur_movie[2].strip())
+        cur_movie[3] = str(cur_movie[3].strip())
+        print(line)
         m = Movie(title=cur_movie[0],year=cur_movie[1],director=cur_movie[2],actors=cur_movie[3],link=cur_movie[4],genre=cur_movie[5],img=cur_movie[6],img_r=cur_movie[7],hd_link=cur_movie[8])
         m.save()
