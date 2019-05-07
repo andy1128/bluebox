054d1b660ff3ff77d618c3ac1d05b7f92c9ac064
Concerned-HumanDev <48697078+Concerned-HumanDev@users.noreply.github.com>
2019-05-07 19:58:55 -0400

    Fixes to Search Bar.

--- bluebox_rework/catalog/views.py
+++ bluebox_rework/catalog/views.py
@@ -4,52 +4,41 @@ from catalog.models import Movie
 from catalog.forms import CheckoutForm, IndexForm
 
 
-
-# Create your views here.
-# def index(request):
-#     return render(request, 'catalog/index.html')
-
 def Index(request):
 
     template_name = 'catalog/index.html'
     form = IndexForm()
     movies = Movie.objects.all()
     
-
-    # print(movies)
-
     args = {'form': form, 'movies': movies}
-
-
-    return render(request, template_name, args)
     if request.method == 'GET':
-        formSearch = IndexForm(request.GET)
-        if formSearch.is_valid():
-            search = formSearch.cleaned_data['search']
+        form = IndexForm(request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
             testVar = search
-        testList = list()
-        for i in Movie.objects.all():
-            if testVar in i.title:
-                testList.append(str(i.img_r))
-            if testVar in i.year:
-                testList.append(str(i.year))
-            if testVar in i.director:
-                if i.img_r not in testList:
+            testList = list()
+            for i in Movie.objects.all():
+                if testVar in i.title:
                     testList.append(str(i.img_r))
-            if testVar in i.actors:
-                if i.img_r not in testList:
-                    testList.append(str(i.img_r))
-            if testVar in i.genre:
-                if i.img_r not in testList:
-                    testList.append(str(i.img_r))
-            if i.title == "Good Will Hunting":
-                print(testList)
+                # if testVar in i.year:
+                #     testList.append(str(i.year))
+                if testVar in i.director:
+                    if i.img_r not in testList:
+                        testList.append(str(i.img_r))
+                if testVar in i.actors:
+                    if i.img_r not in testList:
+                        testList.append(str(i.img_r))
+                if testVar in i.genre:
+                    if i.img_r not in testList:
+                        testList.append(str(i.img_r))
+                if i.title == "Good Will Hunting":
+                    print(testList)
             #return testList
             if len(testList) == 0:
                 print("No movies avaialable")
     
-   
-
+    form = IndexForm()
+    return render(request, template_name, args)
     # formSearch = IndexForm()
     # return render(request, template_name, {'form': formSearch})
 
